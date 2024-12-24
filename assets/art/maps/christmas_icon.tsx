844eef6b6b4c4f6a268a7b4d65ada56f64f0ab6e
yufei <yufeiwu@cs.umu.se>
2024-12-24 15:54:09 +0100

    map, background sound

---
+++ assets/art/maps/christmas_icon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="christmas_icon" tilewidth="32" tileheight="32" spacing="1" margin="2" tilecount="36" columns="3">
+ <image source="../tilesets/christmas_icon.png" width="127" height="411"/>
+</tileset>
